255d7718776fa0688469cc5baeadcf31aa3bee38
Jon <gsoc.student@gmail.com>
2012-06-04 02:37:30 -0400

    SUMMARY: Nearly finished the steganography, spent a long time tracking down a double hashing bug which occurs when there are negative values.
    ADD: Added the HashTable class which provides functions for generating hash table and double hashing
    
    ADD: Added the miller-rabin test, which is an efficient primality test
    
    FIX: Fixed a bug which occurred due to the fact that java does negative modulus incorrectly, for example (modular mathematics) modulus of negative numbers wrap back around, ie. -2 mod 5 == 3, not so in C/C++ world, which I already knew, but assumed java had resolved this issue due to the fact it doesn't have unsigned values! Point is java is fucking stupid about not having unsigned integers and how it handles modulus, they need to take a hint from Python!

---
+++ branches/crypto-dev/Steganography/src/HashTable.java
@@ -0,0 +1,120 @@
+import java.math.BigInteger;
+
+/**
+ * Provides the basic functionality for generating hash tables efficiently
+ * using double hashing
+ */
+public abstract class HashTable
+{
+	/*
+	 *  Constant value used in determining the interval for double hashing
+	 *  it MUST be a prime number to minimize collision
+	 */
+	private static final int CONSTANT_INTERVAL = 5;
+	
+	
+	/*
+	 * Returns the initial probe value for generating the hash table given the
+	 * key
+	 * 
+	 * NOTE: The size of the bucket being probed should be a prime number
+	 * to ensure that the modulus for the probe and constant interval
+	 * are co-prime
+	 * 
+	 * @param key The key to be hashed
+	 * @param bucketSize The size of the bucket(s) being probed, should
+	 * be prime number to ensure maximum efficiency
+	 * 
+	 * @return Returns a probe
+	 */
+	public static int getProbe(int key, int bucketSize)
+	{
+		// Avoid negative values as java does negative modulus wrong... fucking hack
+		return (key < 0) ? (bucketSize - (Math.abs(key) % bucketSize)) : (key % bucketSize);
+	}
+	public static BigInteger getProbe(BigInteger key, int bucketSize)
+	{
+		return key.mod(BigInteger.valueOf(bucketSize));
+	}
+	
+	/*
+	 * Returns the interval for incrementing the probe
+	 */
+	public static int getInterval(int key)
+	{
+		// Avoid negative values as java does negative modulus wrong... fucking hack
+		return (key < 0) ? (CONSTANT_INTERVAL - (((key % CONSTANT_INTERVAL) + CONSTANT_INTERVAL) % CONSTANT_INTERVAL))
+				: (CONSTANT_INTERVAL - (key % CONSTANT_INTERVAL));
+	}
+	public static BigInteger getInterval(BigInteger key)
+	{
+		// One-liner... fucking hack
+		return BigInteger.valueOf(CONSTANT_INTERVAL).subtract(key.mod(BigInteger.valueOf(CONSTANT_INTERVAL)));
+	}
+	
+	
+	/*
+	 * Returns the nearest co-prime value for the size of the bucket(s), this
+	 * is due to a fact of number theory where the number of collisions is
+	 * minimized only if both the constant interval and the bucket size are
+	 * both prime. The two values are only guaranteed to be relatively prime
+	 * if they are BOTH prime.
+	 * 
+	 * @see http://stackoverflow.com/questions/1145217/why-should-hash-functions-use-a-prime-number-modulus
+	 * @see http://mathworld.wolfram.com/RelativelyPrime.html
+	 * 
+	 * NOTE: If the two bucket is already co-prime with the constant interval
+	 * then the size of the bucket is returned.
+	 * 
+	 * NOTE: By using the nearest prime value of the bucket to ensure that it
+	 * is co-prime the number of values selected is less than that of the
+	 * original bucket.
+	 * 
+	 * @param bucketSize The size of the bucket(s)
+	 * @returns The bucketSize if it is already co-prime with the constant
+	 * interval, or the nearest prime number.  
+	 */
+	public static int getCoPrime(int bucketSize)
+	{
+		int a = CONSTANT_INTERVAL;
+		int b = bucketSize;
+		int c = 0;
+		int newBucketSize = 0;
+		
+		// Quickly apply Euclid's Greatest Common Divisor algorithm, if
+		while (b != 0)
+		{
+			c = b;
+			b = a % b;
+			a = c;
+		}
+		
+		// If a is 1 then the bucketSize is co-prime with constant interval
+		if (b == 1)
+		{
+			return bucketSize;
+		}
+		
+		/*
+		 * Otherwise return the nearest prime number as the co-prime, search for
+		 * the next nearest prime according to the Prime number denisty theorem
+		 * that states the probability of finding a prime number is  1/ln(bucketSize)
+		 * 
+		 *  @see http://en.wikipedia.org/wiki/Prime_number_theorem
+		 */
+		for (int i = 1; i <= (int)Math.ceil(Math.log(bucketSize)); ++i)
+		{
+			/*
+			 * Use the miller-rabin test to determine if the new bucketSize is a
+			 * prime number, return it if it is a prime number
+			 * 
+			 * @see http://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+			 */
+			if (MillerRabin32.miller_rabin_32(bucketSize - i))
+			{
+				newBucketSize = bucketSize - i;
+			}
+		}
+		return newBucketSize;
+	}
+}
---
+++ branches/crypto-dev/Steganography/src/MillerRabin32.java
@@ -0,0 +1,65 @@
+/* Copyright (c) 2012 the authors listed at the following URL, and/or
+the authors of referenced articles or incorporated external code:
+http://en.literateprograms.org/Miller-Rabin_primality_test_(Java)?action=history&offset=20111215044139
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Retrieved from: http://en.literateprograms.org/Miller-Rabin_primality_test_(Java)?oldid=17585
+*/
+
+public abstract class MillerRabin32
+{
+    private static int modular_exponent_32(int base, int power, int modulus) {
+        long result = 1;
+        for (int i = 31; i >= 0; i--) {
+            result = (result*result) % modulus;
+            if ((power & (1 << i)) != 0) {
+                result = (result*base) % modulus;
+            }
+        }
+        return (int)result; // Will not truncate since modulus is an int
+    }
+
+
+    private static boolean miller_rabin_pass_32(int a, int n) {
+        int d = n - 1;
+	int s = Integer.numberOfTrailingZeros(d);
+	d >>= s;
+        int a_to_power = modular_exponent_32(a, d, n);
+        if (a_to_power == 1) return true;
+        for (int i = 0; i < s-1; i++) {
+            if (a_to_power == n-1) return true;
+            a_to_power = modular_exponent_32(a_to_power, 2, n);
+        }
+        if (a_to_power == n-1) return true;
+        return false;
+    }
+
+    public static boolean miller_rabin_32(int n) {
+        if (n <= 1) return false;
+        else if (n == 2) return true;
+        else if (miller_rabin_pass_32( 2, n) &&
+            (n <= 7  || miller_rabin_pass_32( 7, n)) &&
+            (n <= 61 || miller_rabin_pass_32(61, n)))
+            return true;
+        else
+            return false;
+    }
+}
