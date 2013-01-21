befa16a44b2e0c9ef0e2779054c801ba247f5d9c
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-01-21 09:12:21 -0500

    Intending to add a delete number option

--- branches/prephase3/src/com/tinfoil/sms/settings/EditNumber.java
+++ branches/prephase3/src/com/tinfoil/sms/settings/EditNumber.java
@@ -17,130 +17,135 @@
 
 package com.tinfoil.sms.settings;
 
-import com.tinfoil.sms.R;
-import com.tinfoil.sms.dataStructures.TrustedContact;
-import com.tinfoil.sms.dataStructures.Number;
-import com.tinfoil.sms.database.DBAccessor;
-import com.tinfoil.sms.utility.MessageService;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class EditNumber extends Activity{
-    
-	public static final String UPDATE = "update";
-	public static final String NEW = "new_number";
-	
-	private EditText phoneNumber;
-	private EditText sharedInfo1;
-	private EditText sharedInfo2;
-	private EditText bookPath;
-	private EditText bookInverse;
-	private Button save;
-	private TrustedContact tc;
-	private String originalNumber;
-	private static int position;
-	
-	@Override
+import com.tinfoil.sms.R;
+import com.tinfoil.sms.dataStructures.Number;
+import com.tinfoil.sms.dataStructures.TrustedContact;
+import com.tinfoil.sms.database.DBAccessor;
+import com.tinfoil.sms.utility.MessageService;
+
+public class EditNumber extends Activity {
+
+    public static final String UPDATE = "update";
+    public static final String NEW = "new_number";
+
+    private EditText phoneNumber;
+    private EditText sharedInfo1;
+    private EditText sharedInfo2;
+    private EditText bookPath;
+    private EditText bookInverse;
+    private Button save;
+    private TrustedContact tc;
+    private String originalNumber;
+    private static int position;
+
+    @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.edit_number);
-        
-        
+
         /* TODO comment
          * TODO consider how to handle the saving aspect
          * - have multiple save buttons (current)
          * - have a single save button (in addContact) and have this as temp passing back the object (not as ideal)
          */
-       
-        phoneNumber = (EditText)findViewById(R.id.phone_number);
-        sharedInfo1 = (EditText)findViewById(R.id.shared_secret_1);
-        sharedInfo2 = (EditText)findViewById(R.id.shared_secret_2);
-        bookPath = (EditText)findViewById(R.id.book_path);
-        bookInverse = (EditText)findViewById(R.id.book_inverse);
-        save = (Button)findViewById(R.id.save);
-        
-        Intent intent = this.getIntent();
-        
-        if(intent != null)
+
+        this.phoneNumber = (EditText) this.findViewById(R.id.phone_number);
+        this.sharedInfo1 = (EditText) this.findViewById(R.id.shared_secret_1);
+        this.sharedInfo2 = (EditText) this.findViewById(R.id.shared_secret_2);
+        this.bookPath = (EditText) this.findViewById(R.id.book_path);
+        this.bookInverse = (EditText) this.findViewById(R.id.book_inverse);
+        this.save = (Button) this.findViewById(R.id.save);
+
+        final Intent intent = this.getIntent();
+
+        if (intent != null)
         {
-        	originalNumber = intent.getStringExtra(AddContact.EDIT_NUMBER);
-        	position = intent.getIntExtra(AddContact.POSITION, AddContact.NEW_NUMBER_CODE);
+            this.originalNumber = intent.getStringExtra(AddContact.EDIT_NUMBER);
+            position = intent.getIntExtra(AddContact.POSITION, AddContact.NEW_NUMBER_CODE);
             this.getIntent().removeExtra(AddContact.EDIT_NUMBER);
             this.getIntent().removeExtra(AddContact.POSITION);
         }
         else
         {
-        	finish();
+            this.finish();
         }
-        
-        if(position != AddContact.NEW_NUMBER_CODE)
+
+        if (position != AddContact.NEW_NUMBER_CODE)
         {
-        	tc = MessageService.dba.getRow(originalNumber);
-        
-	        phoneNumber.setText(originalNumber);        
-	        
-	        sharedInfo1.setText(tc.getNumber(originalNumber).getSharedInfo1());
-	        
-	        sharedInfo2.setText(tc.getNumber(originalNumber).getSharedInfo2());
-	        
-	        bookPath.setText(tc.getNumber(originalNumber).getBookPath());
-	        
-	        bookInverse.setText(tc.getNumber(originalNumber).getBookInversePath());
+            this.tc = MessageService.dba.getRow(this.originalNumber);
+
+            this.phoneNumber.setText(this.originalNumber);
+
+            this.sharedInfo1.setText(this.tc.getNumber(this.originalNumber).getSharedInfo1());
+
+            this.sharedInfo2.setText(this.tc.getNumber(this.originalNumber).getSharedInfo2());
+
+            this.bookPath.setText(this.tc.getNumber(this.originalNumber).getBookPath());
+
+            this.bookInverse.setText(this.tc.getNumber(this.originalNumber).getBookInversePath());
         }
         else
         {
-        	tc = new TrustedContact();    
-	        
-	        sharedInfo1.setText(DBAccessor.DEFAULT_S1);
-	        
-	        sharedInfo2.setText(DBAccessor.DEFAULT_S2);
-	        
-	        bookPath.setText(DBAccessor.DEFAULT_BOOK_PATH);
-	        
-	        bookInverse.setText(DBAccessor.DEFAULT_BOOK_INVERSE_PATH);
-        }
-        
-        save.setOnClickListener(new OnClickListener(){
+            this.tc = new TrustedContact();
 
-			public void onClick(View v) {
-				
-				if(phoneNumber.getText().toString() != null &&
-						phoneNumber.getText().toString().length() > 0)
-				{
-					Number tempNumber = tc.getNumber(originalNumber);
-					tempNumber.setNumber(phoneNumber.getText().toString());
-					tempNumber.setSharedInfo1(sharedInfo1.getText().toString());
-					tempNumber.setSharedInfo2(sharedInfo2.getText().toString());
-					tempNumber.setBookPath(bookPath.getText().toString());
-					tempNumber.setBookInversePath(bookInverse.getText().toString());
-					
-					MessageService.dba.updateNumberRow(tempNumber, originalNumber, 0);
-					
-					Intent data = new Intent();
-					
-					if(tempNumber.getNumber().equalsIgnoreCase(originalNumber))
-					{					
-						data.putExtra(EditNumber.UPDATE, false);					
-					}
-					else
-					{
-						data.putExtra(EditNumber.UPDATE, true);
-					}
-					
-					data.putExtra(EditNumber.NEW, tempNumber.getNumber());
-					data.putExtra(AddContact.POSITION, position);
-					
-					EditNumber.this.setResult(AddContact.REQUEST_CODE, data);
-	
-					EditNumber.this.finish();
-				}
-			}
+            this.sharedInfo1.setText(DBAccessor.DEFAULT_S1);
+
+            this.sharedInfo2.setText(DBAccessor.DEFAULT_S2);
+
+            this.bookPath.setText(DBAccessor.DEFAULT_BOOK_PATH);
+
+            this.bookInverse.setText(DBAccessor.DEFAULT_BOOK_INVERSE_PATH);
+        }
+
+        this.save.setOnClickListener(new OnClickListener() {
+
+            public void onClick(final View v) {
+
+                if (EditNumber.this.phoneNumber.getText().toString() != null &&
+                        EditNumber.this.phoneNumber.getText().toString().length() > 0)
+                {
+                    final Number tempNumber = EditNumber.this.tc.getNumber(EditNumber.this.originalNumber);
+                    tempNumber.setNumber(EditNumber.this.phoneNumber.getText().toString());
+                    tempNumber.setSharedInfo1(EditNumber.this.sharedInfo1.getText().toString());
+                    tempNumber.setSharedInfo2(EditNumber.this.sharedInfo2.getText().toString());
+                    tempNumber.setBookPath(EditNumber.this.bookPath.getText().toString());
+                    tempNumber.setBookInversePath(EditNumber.this.bookInverse.getText().toString());
+
+                    MessageService.dba.updateNumberRow(tempNumber, EditNumber.this.originalNumber, 0);
+
+                    final Intent data = new Intent();
+
+                    if (tempNumber.getNumber().equalsIgnoreCase(EditNumber.this.originalNumber))
+                    {
+                        data.putExtra(EditNumber.UPDATE, false);
+                    }
+                    else
+                    {
+                        data.putExtra(EditNumber.UPDATE, true);
+                    }
+
+                    data.putExtra(EditNumber.NEW, tempNumber.getNumber());
+                    data.putExtra(AddContact.POSITION, position);
+
+                    EditNumber.this.setResult(AddContact.REQUEST_CODE, data);
+
+                    EditNumber.this.finish();
+                }
+            }
         });
-	}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 }
