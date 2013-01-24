1f6fc3fd78425f7d44f3a547fc7b3cc9cbd57dd9
gnu_user <gsoc.student@gmail.com>
2013-01-24 00:00:52 -0500

    Added incomplete ECGKeyExchange unit tests
    Will need to add the ECKeyUtil unit tests before implementing the unit
    tests for this class as they are critical to testing this class.

---
+++ branches/crypto-dev/CryptoLib/src/com/tinfoilsms/test/ECGKeyExchangeTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.tinfoilsms.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author jon
+ *
+ */
+public class ECGKeyExchangeTest
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+		
+		
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2()
+	{
+		fail("Not yet implemented");
+	}
+
+}
