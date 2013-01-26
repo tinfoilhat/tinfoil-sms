5e62d7eac625f90d2158d226bf751e4e191b15ca
gnu_user <gsoc.student@gmail.com>
2013-01-26 16:01:12 -0500

    Added unit tests for key exchange, such as man-in-middle-attack tests
    Added unit tests for ECGKyeExchange, in particular the man-in-middle-attack
    tests are important in verifying that the sign/verify operations work
    correctly to prevent man-in-middle-attacks. They also highlight the security
    weakness if users simply use the default shared info (initiator, recipient).

--- branches/crypto-dev/CryptoLib/src/com/tinfoilsms/test/ECGKeyExchangeTest.java
+++ branches/crypto-dev/CryptoLib/src/com/tinfoilsms/test/ECGKeyExchangeTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.contrib.assumes.*;
+import org.junit.runner.RunWith;
+import org.spongycastle.crypto.DataLengthException;
 import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.params.ECPrivateKeyParameters;
 import org.spongycastle.crypto.params.ECPublicKeyParameters;
@@ -22,6 +25,7 @@ import com.tinfoilsms.crypto.ECKeyParam;
  * cases/scenarios
  *
  */
+@RunWith(Corollaries.class)
 public class ECGKeyExchangeTest
 {
 	private APrioriInfo priorInfo;
@@ -66,9 +70,9 @@ public class ECGKeyExchangeTest
 	}
 
 	/**
-	 * A test which verifies that the recipient receives the signed public
-	 * from the initiator correctly and that the received public key received
-	 * matches the original key sent.
+	 * A test which verifies that the recipient receives the signed public key
+	 * from the initiator correctly and that the public key received matches 
+	 * the original key sent.
 	 */
 	@Test
 	public void KeyExchangeInitiator()
@@ -79,14 +83,14 @@ public class ECGKeyExchangeTest
 																new SHA256Digest(), 
 																AlicetoBobEncodedPubKey, 
 																priorInfo, 
-																true);
+																true);		// Initiator
 		
 		/* Bob (recipient) receives the key and verifies signature */
 		assertTrue(ECGKeyExchange.verifyPubKey(
-				new SHA256Digest(), 
-				AlicetoBobEncodedSignPubKey, 
-				priorInfo, 
-				false));
+						new SHA256Digest(), 
+						AlicetoBobEncodedSignPubKey, 
+						priorInfo, 
+						false));
 		
 		/* Verify that alice's decoded public key is the same as original */
 		ECPublicKeyParameters AlicetoBobPubKey = ECGKeyUtil.decodeSignedPubKey(
@@ -104,10 +108,183 @@ public class ECGKeyExchangeTest
 	}
 	
 	
+	/**
+	 * A test which verifies that the initiator receives the signed public key
+	 * from the recipient correctly and that the public key received matches the
+	 * original key sent.
+	 * 
+	 * Depends on the KeyExchangeInitiator executing successfully
+	 */
 	@Test
-	public void test2()
+	@Assumes({"KeyExchangeInitiator"})
+	public void keyExchangeRecipient()
 	{
-		fail("Not yet implemented");
+		/* Bob (recipient) encodes, signs, and sends public key to Alice (initiator) */
+		byte[] BobtoAliceEncodedPubKey = ECGKeyUtil.encodePubKey(param, bobPubKey);
+		byte[] BobtoAliceEncodedSignPubKey = ECGKeyExchange.signPubKey(
+																new SHA256Digest(), 
+																BobtoAliceEncodedPubKey, 
+																priorInfo, 
+																false);		// Recipient
+		
+		/* Alice (initiator) receives Bob's key and verifies signature */
+		assertTrue(ECGKeyExchange.verifyPubKey(
+						new SHA256Digest(), 
+						BobtoAliceEncodedSignPubKey, 
+						priorInfo, 
+						true));		// Alice was the key exchange initiator
+		
+		
+		/* Verify that Bob's decoded public key, which Alice received is the same as original */
+		ECPublicKeyParameters BobtoAlicePubKey = ECGKeyUtil.decodeSignedPubKey(
+														param, 
+														new SHA256Digest(), 
+														BobtoAliceEncodedSignPubKey);
+		
+		assertTrue(bobPubKey.getQ().getX().toBigInteger().equals(BobtoAlicePubKey.getQ().getX().toBigInteger()));
+		assertTrue(bobPubKey.getQ().getY().toBigInteger().equals(BobtoAlicePubKey.getQ().getY().toBigInteger()));
+		
+		System.out.print("\nBobtoAlicePubKey: \tX = " + BobtoAlicePubKey.getQ().getX().toBigInteger());
+		System.out.println(", Y = " + BobtoAlicePubKey.getQ().getY().toBigInteger());
+		System.out.print("bobPubKey: \t\tX = " + bobPubKey.getQ().getX().toBigInteger());
+		System.out.println(", Y = " + bobPubKey.getQ().getY().toBigInteger());
+	
 	}
 
+
+	/**
+	 * Test which verifies that an exception is thrown when the a priori shared information
+	 * S1 and S2 is not specified (empty)
+	 * 
+	 * Depends on APrioriInfoTest.noSharedInfo,  APrioriInfoTest.oneSharedInfo
+	 */
+	@Test(expected=DataLengthException.class)
+	@Assumes({"APrioriInfoTest.noSharedInfo", "APrioriInfoTest.oneSharedInfo"})
+	public void signNoSharedInfo()
+	{
+		priorInfo = new APrioriInfo("", "");
+		
+		/* Bob (recipient) encodes, signs, and sends git public key to Alice (initiator) */
+		byte[] tempEncodedPubKey = ECGKeyUtil.encodePubKey(param, alicePubKey);
+		byte[] tempEncodedSignPubKey = ECGKeyExchange.signPubKey(
+																new SHA256Digest(), 
+																tempEncodedPubKey, 
+																priorInfo, 
+																true);		// Recipient	
+	}
+	
+	
+	/**
+	 * "Man in the middle attack" test version 1, where an attacker intercepts Alice's key
+	 * and tries to sign it using his own shared information S1/S2.
+	 * 
+	 * This test assumes that alice and bob are smart enough to use unique shared information
+	 * (S1/S2) that they exchange a priori to the key exchange, the attacker mallory will
+	 * try and simply sign alice's key using the default S1/S2 (initiator, recipient) hoping
+	 * that bob will just be using defaults.
+	 */
+	@Test
+	@Assumes({"KeyExchangeInitiator", "KeyExchangeRecipient"})
+	public void manInMiddleAttackVer1()
+	{
+		APrioriInfo secureSharedInfo = new APrioriInfo("fuzzy", "wuzzy"); // maybe not the "most" secure
+		
+		/* Alice (initiator) encodes, signs, and sends her public key to bob */
+		byte[] AlicetoBobEncodedPubKey = ECGKeyUtil.encodePubKey(param, alicePubKey);
+		byte[] AlicetoBobEncodedSignPubKey = ECGKeyExchange.signPubKey(
+																new SHA256Digest(), 
+																AlicetoBobEncodedPubKey, 
+																secureSharedInfo, 
+																true);		// Initiator
+		
+		/* Mallory intercepts alice's signed key and tries to perform a man-in-the-middle
+		 * attack by signing it with the default S1/S2 hoping bob is using defaults!
+		 */
+		ECPublicKeyParameters badPubKey = ECGKeyUtil.decodeSignedPubKey(
+														param, 
+														new SHA256Digest(), 
+														AlicetoBobEncodedSignPubKey);
+		
+		byte[] badEncodedPubKey = ECGKeyUtil.encodePubKey(param, badPubKey);
+		
+		/* Mallory now sign's alice key with default shared info (initiator, recipient) */
+		byte[] badEncodedSignPubKey = ECGKeyExchange.signPubKey(
+														new SHA256Digest(), 
+														AlicetoBobEncodedPubKey, 
+														priorInfo, 	// The default shared info 
+														true);		// Initiator
+		
+		/* Mallory now sends the "bad" man-in-the-middle attack key to bob
+		 * pretending that he is Alice
+		 */
+		
+		/* Bob (recipient) receives the "bad" key from Mallory and verifies the signature
+		 * using the secure shared information (fuzzy, wuzzy) that he and Alice discussed
+		 * prior to initiating the key exchange. The signature will fail, alerting bob to
+		 * a potential man-in-the-middle attack!
+		 */
+		assertFalse(ECGKeyExchange.verifyPubKey(
+						new SHA256Digest(), 
+						badEncodedSignPubKey, 
+						secureSharedInfo, 	// The secure shared info alice and bob used
+						false));	// Recipient
+	}
+	
+	
+	/**
+	 * "Man in the middle attack" test version 2, where an attacker intercepts Alice's key
+	 * and tries to sign it using his own shared information S1/S2.
+	 * 
+	 * This test assumes that alice and bob unfortunately use the default shared information
+	 * (S1/S2) and the attacker mallory will try and simply sign alice's key using the default
+	 * S1/S2 (initiator, recipient) because he knowns that alice and bob will likely just be
+	 * using the defaults.
+	 * 
+	 * This test highlights man-in-the-middle attack vulnerability if using the default shared
+	 * information!
+	 */
+	@Test
+	@Assumes({"KeyExchangeInitiator", "KeyExchangeRecipient"})
+	public void manInMiddleAttackVer2()
+	{		
+		/* Alice (initiator) encodes, signs, and sends her public key to bob */
+		byte[] AlicetoBobEncodedPubKey = ECGKeyUtil.encodePubKey(param, alicePubKey);
+		byte[] AlicetoBobEncodedSignPubKey = ECGKeyExchange.signPubKey(
+																new SHA256Digest(), 
+																AlicetoBobEncodedPubKey, 
+																priorInfo,	// INSECURE default shared info 
+																true);		// Initiator
+		
+		/* Mallory intercepts alice's signed key and tries to perform a man-in-the-middle
+		 * attack by signing it with the default S1/S2 hoping bob is using defaults!
+		 */
+		ECPublicKeyParameters badPubKey = ECGKeyUtil.decodeSignedPubKey(
+														param, 
+														new SHA256Digest(), 
+														AlicetoBobEncodedSignPubKey);
+		
+		byte[] badEncodedPubKey = ECGKeyUtil.encodePubKey(param, badPubKey);
+		
+		/* Mallory now sign's alice key with default shared info (initiator, recipient) */
+		byte[] badEncodedSignPubKey = ECGKeyExchange.signPubKey(
+														new SHA256Digest(), 
+														AlicetoBobEncodedPubKey, 
+														priorInfo, 	// The default shared info 
+														true);		// Initiator
+		
+		/* Mallory now sends the "bad" man-in-the-middle attack key to bob
+		 * pretending that he is Alice
+		 */
+		
+		/* Bob (recipient) receives the "bad" key from Mallory and verifies the signature
+		 * using the INSECURE default shared information (initiator, recipient).
+		 * The signature will be verified successfully, and will NOT alert bob to the
+		 * man-in-the-middle attack!
+		 */
+		assertTrue(ECGKeyExchange.verifyPubKey(
+						new SHA256Digest(), 
+						badEncodedSignPubKey, 
+						priorInfo,	// INSECURE default shared info 
+						false));	// Recipient
+	}
 }
