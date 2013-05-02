3daec7d860a533fc4ec3e8acde42a5547caf24f6
gnu_user <gsoc.student@gmail.com>
2013-05-02 00:29:58 -0400

    Added a test runner, all tests pass currently

---
+++ branches/crypto-dev/CryptoLib/src/com/tinfoilsms/test/AllTests.java
@@ -0,0 +1,35 @@
+/** 
+ * Copyright (C) 2013 Jonathan Gillett, Joseph Heron
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.tinfoilsms.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+        APrioriInfoTest.class, 
+        ECEngineTest.class, 
+        ECGKeyExchangeTest.class,
+        ECGKeyUtilTest.class, 
+        ECKeyParamTest.class, 
+        ECKeyTest.class, 
+        NonceTest.class })
+public class AllTests
+{
+
+}
