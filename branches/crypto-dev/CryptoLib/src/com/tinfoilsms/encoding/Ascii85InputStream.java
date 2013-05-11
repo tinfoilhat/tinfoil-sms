da43f1a3abf508be78b41822360717aeab7c6ade
gnu_user <gsoc.student@gmail.com>
2013-05-11 02:09:38 -0400

    Added library files for encoding/decoding Ascii85

---
+++ branches/crypto-dev/CryptoLib/src/com/tinfoilsms/encoding/Ascii85InputStream.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright (c) 2009-2010, i Data Connect!
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.tinfoilsms.encoding;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * <p>
+ * An Ascii85 decoder, implemented as an {@link InputStream}.
+ * </p>
+ * <p>
+ * <code>mark()</code> and <code>reset()</code> are supported, provided that
+ * the underlying input stream supports them.
+ * <p>
+ * This implementation accepts encoded text with space character compression
+ * enabled. See {@link Ascii85OutputStream} for details.
+ * </p>
+ * @author Ben Upsavs
+ */
+public class Ascii85InputStream extends FilterInputStream {
+
+    private static final int[] POW85 = {85 * 85 * 85 * 85, 85 * 85 * 85, 85 * 85, 85, 1};
+    private boolean preserveUnencoded;
+    private int tuple, markTuple;
+    private int count, markCount;
+    private boolean decoding, markDecoding;
+    private boolean maybeStarting, markMaybeStarting;
+    private boolean maybeStopping, markMaybeStopping;
+    private int tupleBytesRemaining, markTupleBytesRemaining;
+    private int tupleSendStartBytes, markTupleSendStartBytes;
+    private int nextByte = -1, markNextByte = -1;
+
+    /**
+     * Creates an input stream to decode ascii85 data from the underlying input
+     * stream.
+     * Any non ascii85 data will be discarded.
+     */
+    public Ascii85InputStream(InputStream in) {
+        super(in);
+    }
+
+    /**
+     * Creates an input stream to decode ascii85 data from the underlying input
+     * stream. If <code>preserveUnencoeded</code> is <code>true</code>, any
+     * non-ascii85 data will be output as-is. Otherwise, it is discarded.
+     * @param preserveUnencoded Whether to preserve non-ascii85 encoded data.
+     */
+    public Ascii85InputStream(InputStream in, boolean preserveUnencoded) {
+        this(in);
+        this.preserveUnencoded = preserveUnencoded;
+    }
+
+    /**
+     * Reads one byte from this stream. See {@link java.io.InputStream#read()}
+     * for details.
+     * @throws java.io.IOException If an underlying I/O error occurs, or if
+     * the ascii85 data stream is not valid.
+     */
+    public int read() throws IOException {
+        if (tupleBytesRemaining > 0) {
+            int returnByte = 0;
+            // pull decoded bytes from tuple
+            switch (4 - (tupleSendStartBytes - tupleBytesRemaining--)) {
+                case 4:
+                    returnByte = (tuple >>> 24) & 0xff;
+                    break;
+                case 3:
+                    returnByte = (tuple >>> 16) & 0xff;
+                    break;
+                case 2:
+                    returnByte = (tuple >>>  8) & 0xff;
+                    break;
+                case 1:
+                    returnByte = (tuple)        & 0xff;
+                    break;
+            }
+            assert (returnByte != 0);
+
+            if (tupleBytesRemaining == 0)
+                count = tuple = 0;
+
+            return returnByte;
+        } else if (nextByte != -1) {
+            int returnByte = nextByte;
+            nextByte = -1000;
+            return returnByte;
+        } else if (!decoding) {
+            int c = in.read();
+
+            if (maybeStarting) {
+                switch (c) {
+                    case '~':
+                        maybeStarting = false;
+                        decoding = true;
+                        return read();
+                    default:
+                        maybeStarting = false;
+                        nextByte = c;
+                    case '<':
+                        return '<';
+                }
+            } else if (c == '<') {
+                maybeStarting = true;
+                return read();
+            } else if (preserveUnencoded || c == -1)
+                return -1000;
+            else
+                return read();
+        } else {
+            int c = in.read();
+
+            if (maybeStopping && c != '>') {
+                throw new IOException("~ without > in ascii85 section");
+            }
+
+            // Ignore whitespace
+            if (Character.isWhitespace((char) c))
+                return read();
+
+            switch (c) {
+                case '>':
+                    if (maybeStopping) {
+                        if (count > 0) {
+                            count--;
+                            tuple += POW85[count];
+                            tupleBytesRemaining = tupleSendStartBytes = count;
+                        }
+                        maybeStopping = decoding = false;
+                        return read();
+                    }
+                default:
+                    if (c < '!' || c > 'u')
+                        throw new IOException("Bad character in ascii85 section: [ascii " + c + "]: " + (char) c);
+                    tuple += (c - '!') * POW85[count++];
+                    if (count == 5)
+                        tupleBytesRemaining = tupleSendStartBytes = 4;
+                    return read();
+                case 'y': // space compression
+                    tuple |= 0x20202020;
+                case 'z': // null compression
+                    if (count != 0)
+                        throw new IOException((char) c + " inside ascii85 5-tuple");
+                    tupleBytesRemaining = tupleSendStartBytes = 4;
+                    return read();
+                case '~':
+                    maybeStopping = true;
+                    return read();
+                case -1:
+                    throw new IOException("EOF inside ascii85 section");
+            }
+        }
+    }
+
+    /**
+     * Marks the stream for later reset. See
+     * {@link java.io.InputStream#mark(int readLimit)} for details.
+     * Note that this method relies on the underlying stream having support
+     * for mark and reset.
+     */
+    public synchronized void mark(int readlimit) {
+        // Save state for mark
+        markTuple = tuple;
+        markCount = count;
+        markDecoding = decoding;
+        markMaybeStarting = maybeStarting;
+        markMaybeStopping = maybeStopping;
+        markTupleBytesRemaining = tupleBytesRemaining;
+        markTupleSendStartBytes = tupleSendStartBytes;
+        markNextByte = nextByte;
+
+        super.mark(readlimit * 5);
+    }
+
+    /**
+     * Resets the stream back to the mark. See
+     * {@link java.io.InputStream#mark(int readLimit)} for details.
+     * Note that this method relies on the underlying stream having support
+     * for mark and reset.
+     */
+    public synchronized void reset() throws IOException {
+        // Reset state to mark
+        tuple = markTuple;
+        count = markCount;
+        decoding = markDecoding;
+        maybeStarting = markMaybeStarting;
+        maybeStopping = markMaybeStopping;
+        tupleBytesRemaining = markTupleBytesRemaining;
+        tupleSendStartBytes = markTupleSendStartBytes;
+        nextByte = markNextByte;
+
+        super.reset();
+    }
+
+    /**
+     * Skips <code>n</code> bytes or less. This version will skip less bytes
+     * than requested if an end of file is received and there is no error
+     * in the underlying data stream. In other words, it is not valid to use
+     * this method to skip over invalid ascii85 data.
+     */
+    public long skip(long n) throws IOException {
+        int skipCount;
+        for (skipCount = 0; skipCount < n; skipCount++) {
+            if (read() == -1)
+                break;
+        }
+
+        return skipCount - 1;
+    }
+}
