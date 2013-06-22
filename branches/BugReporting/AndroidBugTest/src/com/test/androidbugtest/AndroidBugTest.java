9fb398dbb02399abeeaae9ea6759d0f5363527c0
gnu_user <gsoc.student@gmail.com>
2013-06-21 23:28:39 -0400

    Added the updated to AndroidBugTest to report to ACRAlyzer

--- branches/BugReporting/AndroidBugTest/src/com/test/androidbugtest/AndroidBugTest.java
+++ branches/BugReporting/AndroidBugTest/src/com/test/androidbugtest/AndroidBugTest.java
@@ -13,15 +13,29 @@ import org.acra.*;
 import org.acra.annotation.*;
 
 
+/*
 @ReportsCrashes(formKey = "dFllaFBUVXphdkE4RWFnY3JVcGI3REE6MQ",
 				logcatArguments = { "-t", "200", "-v", "time"}) 
+*/
+
+@ReportsCrashes(
+        formKey = "",
+        formUri = "http://code4peace.dyndns.org:5984/acra-tinfoilsms/_design/acra-storage/_update/report",
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+        formUriBasicAuthLogin="reporter",
+        formUriBasicAuthPassword="reportALLthecrashes987",
+        // Your usual ACRA configuration
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+        )
 
 public class AndroidBugTest extends Application
 {
 	  @Override
 	  public void onCreate()
 	  {
-	      // The following line triggers the initialization of ACRA
+	      /* The following line triggers the initialization of ACRA
 	      ACRA.init(this);
 	      
 	      Map<ReportField, String> bugzillaMap = new HashMap<ReportField, String>();
@@ -37,5 +51,8 @@ public class AndroidBugTest extends Application
 	      // TODO: Add support for important exceptions, such as crypto related exceptions
 	      //ACRA.getErrorReporter().handleException(caughtException);
 	      super.onCreate();
+	      */
+	      super.onCreate();
+	      ACRA.init(this);
 	  }
 }
