e2dbdffd568217b968b186672889fce3d256a34d
Joseph <heron.joseph@gmail.com>
2013-08-23 21:55:12 -0400

    REFACTORED: project layout
    Removed crypto-dev and moved tinfoil-sms into the main folder.

--- branches/crypto-dev/strippedcastle/src/org/spongycastle/crypto/tls/TlsSignerCredentials.java
+++
@@ -1,8 +0,0 @@
-package org.spongycastle.crypto.tls;
-
-import java.io.IOException;
-
-public interface TlsSignerCredentials extends TlsCredentials
-{
-    byte[] generateCertificateSignature(byte[] md5andsha1) throws IOException;
-}
