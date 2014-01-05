e93ab62e7bf1368be81e04798ebd1d4c7d5c8a99
Joseph <heron.joseph@gmail.com>
2014-01-05 17:42:34 -0500

    FIXED: send new message limit
    Forgot to remove the limit on sending new messages to contacts.

--- src/com/tinfoil/sms/adapter/MessageBoxWatcher.java
+++ src/com/tinfoil/sms/adapter/MessageBoxWatcher.java
@@ -17,8 +17,6 @@
 
 package com.tinfoil.sms.adapter;
 
-import com.tinfoil.sms.utility.SMSUtility;
-
 import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -26,14 +24,12 @@ import android.widget.TextView;
 
 public class MessageBoxWatcher implements TextWatcher {
 	private TextView wordCount;
-	private boolean trusted;
 	private int wordCounter;
 	private static final int START_NUMBER = 5;
 	
 	public MessageBoxWatcher(Activity app, int id, boolean trusted)
 	{
 		wordCount = (TextView) app.findViewById(id);
-		this.trusted = trusted;
 	}
 	
 	public void afterTextChanged(Editable s) {
@@ -48,18 +44,7 @@ public class MessageBoxWatcher implements TextWatcher {
 
 		wordCounter = s.length();
 		int limit = 0;
-		
-		if(trusted)
-		{
-			limit = SMSUtility.ENCRYPTED_MESSAGE_LENGTH;
-		}
-		else
-		{
-			limit = SMSUtility.MESSAGE_LENGTH;
-		}
-		
-		//TODO adjust so that once it gets larger than the message size limit show D/N where D = number of characters and N = number of messages 
-		
+				
 		if(wordCounter > START_NUMBER)
 		{
 			wordCount.setText(String.valueOf(limit - wordCounter));
