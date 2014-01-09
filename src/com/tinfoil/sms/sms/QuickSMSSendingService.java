42fb38c286f9fd900db7ecf27c9dcf95026a119e
Joseph <heron.joseph@gmail.com>
2014-01-09 15:20:00 -0500

    ADDED: support for android 4.4 SMS default sms
    Added the neccessary permissions and classes required by android 4.4 to allow for an app to become the default SMS application. However, MMS and Send response via message are not implemented and will through unimplemented errors.

---
+++ src/com/tinfoil/sms/sms/QuickSMSSendingService.java
@@ -0,0 +1,16 @@
+package com.tinfoil.sms.sms;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class QuickSMSSendingService extends Service {
+	public QuickSMSSendingService() {
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}
