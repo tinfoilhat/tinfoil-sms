ccc607667b34999eaa32e043f34189902ad06aaa
gnu_user <gsoc.student@gmail.com>
2014-01-12 18:53:44 -0500

    Updated the counter to give a better limit, instead of always negative
    The limit is approximately 88 chars with encryption enabled for it to
    still be 1 SMS message however 100 is a rounder number...

--- src/com/tinfoil/sms/adapter/MessageBoxWatcher.java
+++ src/com/tinfoil/sms/adapter/MessageBoxWatcher.java
@@ -20,6 +20,7 @@ package com.tinfoil.sms.adapter;
 import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,7 +28,10 @@ public class MessageBoxWatcher implements TextWatcher {
     private ImageView sendSMS;
 	private TextView wordCount;
 	private int wordCounter;
-	private static final int START_NUMBER = 5;
+	
+	// The limit for it to still be one message is 120 - 32 =~ 88
+	private static final int LIMIT = 100;
+	private static final int START_NUMBER = 64;
 	
 	public MessageBoxWatcher(Activity app, int buttonId, int countId)
 	{
@@ -52,20 +56,22 @@ public class MessageBoxWatcher implements TextWatcher {
 		{
 			wordCounter--;
 		}
+		
+		if (s.length() <= START_NUMBER)
+		{
+		    wordCount.setText("");
+	        wordCount.setVisibility(View.GONE);  
+		}
 	}
 
 	public void beforeTextChanged(CharSequence s, int start, int count, int after) 
 	{
 		wordCounter = s.length();
-		int limit = 0;
-        
+
 		if(wordCounter > START_NUMBER)
 		{
-			wordCount.setText(String.valueOf(limit - wordCounter));
-		}
-		else
-		{
-			wordCount.setText("");
+		    wordCount.setVisibility(View.VISIBLE);
+			wordCount.setText(String.valueOf(LIMIT - wordCounter));
 		}
 	}
 
