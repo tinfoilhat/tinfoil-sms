5fc3292480e40dbd713364c0578bc285b406c6e5
Joseph <heron.joseph@gmail.com>
2014-05-01 17:57:27 -0400

    FIXED: message limit issue
    Message limit was built to support only one contact (making use of sqlite's 'min' function). It will now properly update the messages (delete old ones in order to maintain the max number of messages). However, 1 is the lowest message limit (0 is still accepted but is treated as 1). Finally, messages are not 'actively' deleted, which means if the threshold is lower then the previous threshold there will be no apparent changes (the oldest message will be deleted and replaced by the newest one).

--- src/com/tinfoil/sms/database/DBAccessor.java
+++ src/com/tinfoil/sms/database/DBAccessor.java
@@ -276,24 +276,24 @@ public class DBAccessor {
         if (cur.moveToFirst() && cur.getCount() >= Integer.valueOf(sharedPrefs.getString
         		(QuickPrefsActivity.MESSAGE_LIMIT_SETTING_KEY, String.valueOf(SMSUtility.LIMIT))))
         {
-        	Cursor date_cur = context.getContentResolver().query(
+        	/*Cursor date_cur = context.getContentResolver().query(
         			DatabaseProvider.MESSAGE_CONTENT_URI, new String[]
         			{"MIN("+SQLitehelper.KEY_DATE+")"},
             		null, null, null);
         	
         	if (date_cur.moveToFirst() && date_cur.getLong(0) < message.getDate())
-        	{
+        	{*/
         		/*
 		    	 * Updated the update db sql command to account for messages having the exact same date
 		    	 */
         		context.getContentResolver().update(DatabaseProvider.MESSAGE_CONTENT_URI,
-        				cv, SQLitehelper.KEY_ID + " = " + "(SELECT id FROM " +
-        				SQLitehelper.MESSAGES_TABLE_NAME + " WHERE "+ SQLitehelper.KEY_DATE
-        				+ " = " + "(SELECT MIN("+SQLitehelper.KEY_DATE+") FROM " 
-		        		+ SQLitehelper.MESSAGES_TABLE_NAME + ") LIMIT 1)", null);
+        				cv, SQLitehelper.KEY_ID + " = (SELECT " + SQLitehelper.KEY_ID + " FROM "
+        				+ SQLitehelper.MESSAGES_TABLE_NAME + " WHERE " 
+        				+ SQLitehelper.KEY_REFERENCE + " = " + reference
+        				+ " ORDER by " + SQLitehelper.KEY_DATE + " DESC LIMIT 1)", null);
         		
-        	}
-        	date_cur.close();
+        	//}
+        	//date_cur.close();
         }
         else
         {
